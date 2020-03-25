912977a4ba54040d8c57b242baf080b288074726
Sam Hamburger <sh786@cornell.edu>
2020-03-25 11:17:59 -0400

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -79,10 +79,10 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#09d3ac`,
+        theme_color: `#09d3ac`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/port-logo.png`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-transition-link`,
