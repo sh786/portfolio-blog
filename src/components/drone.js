b84996902682c524720bce50ad27c4ab6479732d
Sam Hamburger <samhamburger@Sams-MacBook-Pro-2.local>
2020-04-21 18:48:17 -0400

    update styling

--- src/components/drone.js
+++ src/components/drone.js
@@ -18,6 +18,10 @@ const Drone = () => {
           />
         </div>
         <h3 className="drone-item-title">Pittsburgh</h3>
+        <p>Filmed: April, 2020</p>
+        <p>
+          Aerial shots from Point Park of Heinz Field and downtown Pittsburgh.
+        </p>
       </div>
       <div className="drone-item">
         <div className="drone-item-img-container">
@@ -32,6 +36,8 @@ const Drone = () => {
           />
         </div>
         <h3 className="drone-item-title">Puerto Rico</h3>
+        <p>Filmed: May, 2019</p>
+        <p>Drone footage of TPC Dorado and Isla Verde beach.</p>
       </div>
       <div className="drone-item">
         <div className="drone-item-img-container">
@@ -46,6 +52,10 @@ const Drone = () => {
           />
         </div>
         <h3 className="drone-item-title">Beverly, MA</h3>
+        <p>Filmed: August, 2018</p>
+        <p>
+          Flyovers from the Atlantic Ocean near West Beach in Beverly Farms.
+        </p>
       </div>
     </div>
   )
